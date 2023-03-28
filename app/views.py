2ab08be076e70a8026884c672d50e6652f839841
Amaresh8 <amaresh0426@gmail.com>
2023-03-28 20:41:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Amaresh(request):
+    return render(request,'Amaresh.html')
+def Child(request):
+    return render(request,'child.html')
